ab7f384ca98df3aa8eb8f8b1bd7bdeeb240ba5ce
Haidar0096 <haidarrmehsen@gmail.com>
2023-01-27 02:41:42 +0200

    formatted code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -49,8 +49,8 @@ class _MyAppState extends State<MyApp> {
 
     // set a callback to be called when image processing progress changes
     WidgetsBinding.instance.addPostFrameCallback(
-          (timeStamp) async => await _wand.magickSetProgressMonitor(
-            (info, offset, size, clientData) => setState(() {
+      (timeStamp) async => await _wand.magickSetProgressMonitor(
+        (info, offset, size, clientData) => setState(() {
           _progress = offset;
           _maxProgress = size;
           status = '[${info.split('/').first}, $offset, $size, $clientData]';
@@ -70,206 +70,206 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-    home: Builder(builder: (context) {
-      final double displayImageWidth =
-          MediaQuery.of(context).size.width / 2.5;
-      final double displayImageHeight =
-          MediaQuery.of(context).size.height / 2.5;
-      return Scaffold(
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Column(
+        home: Builder(builder: (context) {
+          final double displayImageWidth =
+              MediaQuery.of(context).size.width / 2.5;
+          final double displayImageHeight =
+              MediaQuery.of(context).size.height / 2.5;
+          return Scaffold(
+            body: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            const Text("Input Image",
-                                style: TextStyle(fontSize: 20)),
-                            SizedBox(
-                              width: displayImageWidth,
-                              height: displayImageHeight / 2.5,
-                              child: _inputFile != null
-                                  ? Text(
-                                _inputFile!.path,
-                                textAlign: TextAlign.center,
-                              )
-                                  : const Text(
-                                "No file selected",
-                                textAlign: TextAlign.center,
-                              ),
+                            Column(
+                              children: [
+                                const Text("Input Image",
+                                    style: TextStyle(fontSize: 20)),
+                                SizedBox(
+                                  width: displayImageWidth,
+                                  height: displayImageHeight / 2.5,
+                                  child: _inputFile != null
+                                      ? Text(
+                                          _inputFile!.path,
+                                          textAlign: TextAlign.center,
+                                        )
+                                      : const Text(
+                                          "No file selected",
+                                          textAlign: TextAlign.center,
+                                        ),
+                                ),
+                                _inputFile != null
+                                    ? Container(
+                                        decoration: BoxDecoration(
+                                          border:
+                                              Border.all(color: Colors.black),
+                                        ),
+                                        child: Image.file(
+                                          _inputFile!,
+                                          width: displayImageWidth,
+                                          height: displayImageHeight,
+                                        ),
+                                      )
+                                    : Container(
+                                        width: displayImageWidth,
+                                        height: displayImageHeight,
+                                        color: Colors.grey,
+                                        child: const Center(
+                                            child: Text('No image selected')),
+                                      ),
+                              ],
                             ),
-                            _inputFile != null
-                                ? Container(
-                              decoration: BoxDecoration(
-                                border:
-                                Border.all(color: Colors.black),
-                              ),
-                              child: Image.file(
-                                _inputFile!,
-                                width: displayImageWidth,
-                                height: displayImageHeight,
-                              ),
-                            )
-                                : Container(
-                              width: displayImageWidth,
-                              height: displayImageHeight,
-                              color: Colors.grey,
-                              child: const Center(
-                                  child: Text('No image selected')),
+                            const SizedBox(width: 20),
+                            Column(
+                              children: [
+                                const Text("Output Image",
+                                    style: TextStyle(fontSize: 20)),
+                                SizedBox(
+                                  width: displayImageWidth,
+                                  height: displayImageHeight / 2.5,
+                                  child: _outputFile != null
+                                      ? Text(
+                                          _outputFile!.path,
+                                          textAlign: TextAlign.center,
+                                        )
+                                      : const Text(
+                                          "No file selected",
+                                          textAlign: TextAlign.center,
+                                        ),
+                                ),
+                                _outputFile != null
+                                    ? Container(
+                                        decoration: BoxDecoration(
+                                          border:
+                                              Border.all(color: Colors.black),
+                                        ),
+                                        child: Image.memory(
+                                          _outputFile!.readAsBytesSync(),
+                                          width: displayImageWidth,
+                                          height: displayImageHeight,
+                                        ),
+                                      )
+                                    : Container(
+                                        width: displayImageWidth,
+                                        height: displayImageHeight,
+                                        color: Colors.grey,
+                                        child: const Center(
+                                            child: Text('No image selected')),
+                                      ),
+                              ],
                             ),
                           ],
                         ),
-                        const SizedBox(width: 20),
-                        Column(
-                          children: [
-                            const Text("Output Image",
-                                style: TextStyle(fontSize: 20)),
-                            SizedBox(
-                              width: displayImageWidth,
-                              height: displayImageHeight / 2.5,
-                              child: _outputFile != null
-                                  ? Text(
-                                _outputFile!.path,
-                                textAlign: TextAlign.center,
-                              )
-                                  : const Text(
-                                "No file selected",
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                            _outputFile != null
-                                ? Container(
-                              decoration: BoxDecoration(
-                                border:
-                                Border.all(color: Colors.black),
-                              ),
-                              child: Image.memory(
-                                _outputFile!.readAsBytesSync(),
-                                width: displayImageWidth,
-                                height: displayImageHeight,
-                              ),
-                            )
-                                : Container(
-                              width: displayImageWidth,
-                              height: displayImageHeight,
-                              color: Colors.grey,
-                              child: const Center(
-                                  child: Text('No image selected')),
-                            ),
-                          ],
+                      ),
+                    ),
+                    Text(
+                      'Status: $status',
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Operation time: ${_operationTimeInMillis ?? 0} ms',
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () async {
+                            final filePickerResult = await FilePicker.platform
+                                .pickFiles(
+                                    allowedExtensions: ['jpg', 'jpeg', 'png'],
+                                    type: FileType.custom);
+                            if (filePickerResult != null) {
+                              setState(() {
+                                _inputFile =
+                                    File(filePickerResult.files[0].path!);
+                              });
+                            }
+                          },
+                          child: const Text('pick input image'),
+                        ),
+                        const SizedBox(width: 10),
+                        ElevatedButton(
+                          onPressed: () async {
+                            final directoryPickerResult =
+                                await FilePicker.platform.getDirectoryPath();
+                            if (directoryPickerResult != null) {
+                              setState(() {
+                                _outputDirectory =
+                                    Directory(directoryPickerResult);
+                              });
+                            }
+                          },
+                          child: const Text('pick output directory'),
                         ),
                       ],
                     ),
-                  ),
-                ),
-                Text(
-                  'Status: $status',
-                  style: const TextStyle(fontSize: 20),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  'Operation time: ${_operationTimeInMillis ?? 0} ms',
-                  style: const TextStyle(fontSize: 20),
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
+                    const SizedBox(height: 10),
                     ElevatedButton(
-                      onPressed: () async {
-                        final filePickerResult = await FilePicker.platform
-                            .pickFiles(
-                            allowedExtensions: ['jpg', 'jpeg', 'png'],
-                            type: FileType.custom);
-                        if (filePickerResult != null) {
-                          setState(() {
-                            _inputFile =
-                                File(filePickerResult.files[0].path!);
-                          });
-                        }
-                      },
-                      child: const Text('pick input image'),
+                      onPressed: isLoading
+                          ? null
+                          : () async {
+                              if (_inputFile == null) {
+                                setState(() {
+                                  status = 'Error: input file is null';
+                                });
+                                return;
+                              }
+                              if (_outputDirectory == null) {
+                                setState(() {
+                                  status = 'Error: output directory is null';
+                                });
+                                return;
+                              }
+                              // request permission if not granted
+                              if (!await Permission.storage
+                                  .request()
+                                  .isGranted) {
+                                setState(() {
+                                  status =
+                                      'Error: storage permission is not granted';
+                                });
+                                return;
+                              }
+                              final stopwatch = Stopwatch()..start();
+                              status = await _handlePress();
+                              stopwatch.stop();
+                              print(
+                                  "operation time: ${stopwatch.elapsedMilliseconds}ms");
+                              _operationTimeInMillis =
+                                  stopwatch.elapsedMilliseconds;
+                              setState(() {});
+                            },
+                      child: const Text('Start Processing'),
                     ),
-                    const SizedBox(width: 10),
-                    ElevatedButton(
-                      onPressed: () async {
-                        final directoryPickerResult =
-                        await FilePicker.platform.getDirectoryPath();
-                        if (directoryPickerResult != null) {
-                          setState(() {
-                            _outputDirectory =
-                                Directory(directoryPickerResult);
-                          });
-                        }
-                      },
-                      child: const Text('pick output directory'),
-                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      height: displayImageHeight / 9,
+                      width: MediaQuery.of(context).size.width / 2,
+                      child: isLoading
+                          ? LinearProgressIndicator(
+                              value: (_progress ?? 0) / (_maxProgress ?? 1),
+                              color: Colors.blue,
+                              backgroundColor: Colors.grey,
+                            )
+                          : Container(),
+                    )
                   ],
                 ),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: isLoading
-                      ? null
-                      : () async {
-                    if (_inputFile == null) {
-                      setState(() {
-                        status = 'Error: input file is null';
-                      });
-                      return;
-                    }
-                    if (_outputDirectory == null) {
-                      setState(() {
-                        status = 'Error: output directory is null';
-                      });
-                      return;
-                    }
-                    // request permission if not granted
-                    if (!await Permission.storage
-                        .request()
-                        .isGranted) {
-                      setState(() {
-                        status =
-                        'Error: storage permission is not granted';
-                      });
-                      return;
-                    }
-                    final stopwatch = Stopwatch()..start();
-                    status = await _handlePress();
-                    stopwatch.stop();
-                    print(
-                        "operation time: ${stopwatch.elapsedMilliseconds}ms");
-                    _operationTimeInMillis =
-                        stopwatch.elapsedMilliseconds;
-                    setState(() {});
-                  },
-                  child: const Text('Start Processing'),
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  height: displayImageHeight / 9,
-                  width: MediaQuery.of(context).size.width / 2,
-                  child: isLoading
-                      ? LinearProgressIndicator(
-                    value: (_progress ?? 0) / (_maxProgress ?? 1),
-                    color: Colors.blue,
-                    backgroundColor: Colors.grey,
-                  )
-                      : Container(),
-                )
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        }),
       );
-    }),
-  );
 
   // read an image, do some operations on it, then save it
   Future<String> _handlePress() async {
@@ -325,7 +325,7 @@ class _MyAppState extends State<MyApp> {
 
   void _throwWandExceptionIfExists(MagickWand wand) {
     MagickGetExceptionResult e =
-    _wand.magickGetException(); // get the exception if any
+        _wand.magickGetException(); // get the exception if any
     if (e.severity != ExceptionType.UndefinedException) {
       throw e;
     }
